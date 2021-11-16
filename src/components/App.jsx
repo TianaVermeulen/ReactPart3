c65455f7e6bfb90b64187ae45d509247fdeb2775
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-11-16 12:50:25 +0200

    Using Ternary Operator to make expretion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = true;
+
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+    {isLoggedIn === true ? <h1>Hello</h1> : <Login />}
     </div>
   );
-}
+};
 
 export default App;
