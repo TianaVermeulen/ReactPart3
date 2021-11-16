1d969b4b534f563fe5d4f72bdc7e7f67017876c5
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-11-16 12:58:03 +0200

    React Conditional Rendering using && Operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Login from "./Login";
 
 var isLoggedIn = true;
-
+var currentTime = new Date().getHours();
 
 function App() {
   return (
     <div className="container">
-    {isLoggedIn === true ? <h1>Hello</h1> : <Login />}
+    {/* Ternary}
+    {/* {isLoggedIn === true ? <h1>Hello</h1> : <Login />} */}
+    {/* AND Operator */}
+    {currentTime > 11 && <h1>Why are you still working?</h1>}
+    
+    
     </div>
   );
 };
