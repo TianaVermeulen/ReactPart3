ad79f8dd5224b58d023665585bd25362a88aa8db
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-11-16 13:41:57 +0200

    React Hook - useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import Form from "./Form";
-
-var userIsRegistered = true;
 
 function App() {
-  return (
-    <div className="container">
-      <Form 
-        isRegistered={userIsRegistered}
-      />
-    </div>
-  );
+  return <div />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-//Challenge: Without moving the userIsRegistered variable,
-//1. Show Login as the button text if userIsRegistered is true.
-//Show Register as the button text if userIsRegistered is false.
-//2. Only show the Confirm Password input if userIsRegistered is false.
-//Don't show it if userIsRegistered is true.
+ReactDOM.render(
+  <div className="container">
+    <h1>0</h1>
+    <button>+</button>
+  </div>,
+  document.getElementById("root")
+);
