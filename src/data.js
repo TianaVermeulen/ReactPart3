40d1b1f2b9cb8d9d94fa12316841e9378bf4a834
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-11-17 14:10:23 +0200

    Javascript ES6 Object & Array Destructuring End file

--- src/data.js
+++ src/data.js
@@ -2,4 +2,15 @@ const animals = [
     { name: "cat", sound: "meow" },
     { name: "dog", sound: "woof" }
   ];
-  
+
+  function useAnimal(animal){
+    return [
+      animal.name,
+      function (){
+        console.log(animal.sound)
+      }
+    ]
+  };
+
+  export default animals;
+  export {useAnimal};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
+// import animals, { useAnimal } from "./data";
+
+// // Destuctured array
+
+// const [cat, dog] = animals;
+
+// const [animal, makesound] = useAnimal;
+// makesound();
+
+// // Destructured object
+
+// const {name, sound} = cat;
+
+// const {name: catName, sound: catSound} = cat;
+
+// const {name = "Fluffy", sound = "Purr"} = cat;
+
+// const {name, sound, feedingRequirements: {food, water}} = cat;
+
+
+
 // CHALLENGE: uncomment the code below and see the car stats rendered
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const {honda, tesla} = cars;
+
+const {speedStats:{topSpeed: hondaTopSpeed}} = honda;
+const {speedStats:{topSpeed: teslaTopSpeed}} = tesla;
+
+const {coloursByPopularity: [hondaTopColour]} = honda;
+const {coloursByPopularity: [teslaTopColour]} = tesla;
 
 ReactDOM.render(
   <table>
