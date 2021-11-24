f776b882dbaddf72a8679b7956fa6126882d97c9
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-11-24 14:13:47 +0200

    Start Code for Practice of Complex State

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,48 @@
 import React, { useState } from "react";
 
-function App() {
+  // const [fullName, setFullName] = useState({
+  //   fName: "",
+  //   lName: ""
+  // });
 
-  const [fullName, setFullName] = useState({
-    fName: "",
-    lName: ""
-  });
+  // function handleChange(event) {
+  //   const {value, name} = event.target;
 
-  function handleChange(event) {
-    const {value, name} = event.target;
+  //   setFullName((prevValue) => {
+  //     if (name === "fName") {
+  //       return {
+  //         fName: value,
+  //         lName: prevValue.lName
+  //       }
+  //     } else if (name === "lName"){
+  //       return {
+  //         fName: prevValue.fName,
+  //         lName: value
+  //       }
+  //     }
+  //   });
 
-    setFullName((prevValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName
-        }
-      } else if (name === "lName"){
-        return {
-          fName: prevValue.fName,
-          lName: value
-        }
-      }
+  function App() {
+    const [contact, setContact] = useState({
+      fName: "",
+      lName: "",
+      email: ""
     });
+  
+    return (
+      <div className="container">
+        <h1>
+          Hello {contact.fName} {contact.lName}
+        </h1>
+        <p>{contact.email}</p>
+        <form>
+          <input name="fName" placeholder="First Name" />
+          <input name="lName" placeholder="Last Name" />
+          <input name="email" placeholder="Email" />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
   }
-
-  return (
-    <div className="container">
-      <h1>Hello {fullName.fName} {fullName.lName}</h1>
-      <form>
-        <input name="fName" onChange={handleChange} placeholder="First Name" value={fullName.fName} />
-        <input name="lName" onChange={handleChange} placeholder="Last Name" value={fullName.lName} />
-        <button>Submit</button>
-      </form>
-    </div>
-  );
-}
 
 export default App;
