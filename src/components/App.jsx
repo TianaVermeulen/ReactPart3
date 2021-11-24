fbac8a860e12d9d0d247a697f8f9b897b61b8613
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-11-24 14:50:23 +0200

    Practice Code for JS ES6 Spread Operator - Start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
 // const [fullName, setFullName] = useState({
 //   fName: "",
@@ -22,54 +22,78 @@ import React, { useState } from "react";
 //     }
 //   });
 
-//CHALLENGE: Make the code in App.jsx work.
-//The final app should have a single contact
-//with fName, lName and email.
 
-//HINT: You'll need to apply the following things you learnt:
-//1. Using JS Objects with state.
-//2. Making use of previous state when changing state.
-//3. Working with forms in React.
-//4. Handing events
+// function App() {
+//   const [contact, setContact] = useState({
+//     fName: "",
+//     lName: "",
+//     email: ""
+//   });
+
+//   function handelChange(event) {
+//     const { value, name } = event.target;
+
+//     setContact((prevValue) => {
+//       return {
+//       ...prevValue,
+//       [name]:value
+//       };
+//     });
+//   }
+
+//   return (
+//     <div className="container">
+//       <h1>
+//         Hello {contact.fName} {contact.lName}
+//       </h1>
+//       <p>{contact.email}</p>
+//       <form>
+//         <input name="fName" 
+//         onChange={handelChange} 
+//         placeholder="First Name" 
+//         value={contact.fName} />
+//         <input name="lName" 
+//         onChange={handelChange} 
+//         placeholder="Last Name" 
+//         value={contact.lName} />
+//         <input name="email" 
+//         onChange={handelChange} 
+//         placeholder="Email" 
+//         value={contact.email} />
+//         <button>Submit</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
+
+import React from "react";
+
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
-  });
-
-  function handelChange(event) {
-    const { value, name } = event.target;
-
-    setContact((prevValue) => {
-      return {
-      ...prevValue,
-      [name]:value
-      };
-    });
-  }
-
   return (
     <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input name="fName" 
-        onChange={handelChange} 
-        placeholder="First Name" 
-        value={contact.fName} />
-        <input name="lName" 
-        onChange={handelChange} 
-        placeholder="Last Name" 
-        value={contact.lName} />
-        <input name="email" 
-        onChange={handelChange} 
-        placeholder="Email" 
-        value={contact.email} />
-        <button>Submit</button>
-      </form>
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" />
+        <button>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          <li>A Item </li>
+        </ul>
+      </div>
     </div>
   );
 }
